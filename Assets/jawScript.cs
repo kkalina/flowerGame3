94246a2460b8039c531457102d775677fb19bde1
kkalina <kkalina@umich.edu>
2015-10-18 14:58:19 -0400

    fixed SpeedTree

---
+++ Assets/jawScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jawScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.tag == "Player")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
