cbee17bd6d9ba12471de713a50ada62a453d2a26
kkalina <kkalina@umich.edu>
2015-10-22 14:53:13 -0400

    stuff

---
+++ Assets/ADSsound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ADSsound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.GetComponent<AudioSource>().Play();
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
